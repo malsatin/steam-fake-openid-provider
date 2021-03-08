8ef5e4a970cc601323e09a56b2734796d973b37d
Sergey <9138285+malsatin@users.noreply.github.com>
2021-03-08 13:59:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
-var express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 
-var app = express();
+const DOMAIN = 'localhost';
+
+const app = express();
+
+const privateKey = fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/privkey.pem`, 'utf8');
+const certificate = fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/cert.pem`, 'utf8');
+const ca = fs.readFileSync(`/etc/letsencrypt/live/${DOMAIN}/chain.pem`, 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 /**
  * Discovery endpoint
- * Requires following identity format: http://localhost:3000/id/<steamid>
  */
-
 app.get('/id/:steamid', function(req, res) {
+	console.log('Discovery endpoint called:', req.url);
 
-  res.set('Content-Type', 'application/xrds+xml;charset=utf-8');
-
-  res.send(`
+	res.set('Content-Type', 'application/xrds+xml;charset=utf-8');
+	res.send(`
 <?xml version="1.0" encoding="UTF-8"?>
 <xrds:XRDS xmlns:xrds="xri://$xrds" xmlns="xri://$xrd*($v*2.0)">
 	<XRD>
 		<Service priority="0">
 			<Type>http://specs.openid.net/auth/2.0/signon</Type>
-			<URI>http://localhost:3000</URI>
+			<URI>https://${DOMAIN}</URI>
 		</Service>
 	</XRD>
 </xrds:XRDS>
-  `)
-
+	`);
 });
 
 /**
  * Validation endpoint
  * We simply validate every incoming request
  */
+app.post('/*', function(req, res) {
+	console.log('Validation endpoint called:', req.url);
 
-app.post('/', function(req, res) {
-
-  res.send('ns:http://specs.openid.net/auth/2.0\nis_valid:true\n');
-
+	res.send('ns:http://specs.openid.net/auth/2.0\nis_valid:true\n');
 });
 
 
 /**
  * Run the server
  */
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-app.listen(3000);
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
